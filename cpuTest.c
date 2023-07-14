8ec3ad395585d280f052bbb3c0d722313dc1b7b7
zw <644988358@qq.com>
2023-07-14 11:35:22 +0800

    初始化工程

---
+++ cpuTest.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <sys/time.h>
+
+void time_printA()
+{
+        struct timeval tv; 
+        gettimeofday(&tv,NULL);
+        printf("threadA microsecond:%ld\n",tv.tv_sec*1000000 + tv.tv_usec);  //微秒
+}
+void time_printB()
+{
+        struct timeval tv; 
+        gettimeofday(&tv,NULL);
+        printf("threadB microsecond:%ld\n",tv.tv_sec*1000000 + tv.tv_usec);  //微秒
+}
+
+void *funA(void *arg)
+{
+        printf("I'm thread, Thread ID = %lu\n", pthread_self());
+        while(1)
+        {   
+                time_printA();
+                usleep (1000);
+               
+        } 
+        return NULL;
+}
+
+void *funB(void *arg)
+{
+        printf("I'm thread 1, Thread ID = %lu\n", pthread_self());
+        while(1)
+        {   
+                time_printB();
+                usleep(1000);
+        }   
+        return NULL;
+}
+
+int main()
+{
+        pthread_t tid;
+        pthread_t tid1;
+
+        pthread_create(&tid, NULL, funA, NULL);
+        pthread_create(&tid, NULL, funB, NULL);
+
+        void *status;
+        pthread_join(tid, &status);
+
+        return 0;
+}
